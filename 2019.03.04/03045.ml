3936b1d5798161c32ddd619f6f8445b37db6872c
Sergey <sergeynazarov05@gmail.com>
2019-04-04 23:29:24 +0300

    qwerty

--- 2019.03.04/03045.ml
+++ 2019.03.04/03045.ml
@@ -11,9 +11,13 @@ for i = 0 to ((n/2)-2) do
 print_string ((String.make (n/2) ' ')^"*\n")
 done;
 print_string ((String.make n '*')^"\n");
-for i = 0 to ((n/2)-2) do
+for i = 0 to ((n/2)-3-(n/4)) do
 print_string ((String.make (n/2) ' ')^"*\n")
 done;
+ for i = 0 to ((n/4)-1) do
+print_string ((String.make ((n/4)+i-2)  ' ')^"*"^(String.make  ((n/2)-(n/4)-i+1) ' ')^"*"^(String.make  ((n/2)-(n/4)-i+1) ' ')^"*\n")
+done;
+print_string ((String.make (n/2) ' ')^"*\n");
 print_string ((String.make n '*')^"\n");;
 
 gold n;;
