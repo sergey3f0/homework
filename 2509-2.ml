b77561dced194da15d2af7bf72a9ffa4ec5e4831
sergey3f0 <49240504+sergey3f0@users.noreply.github.com>
2019-10-15 21:07:21 +0300

    Update 2509-2.ml
    нужен файл anna.txt из которого читается текст

--- 2509-2.ml
+++ 2509-2.ml
@@ -9,11 +9,13 @@ x:= !x^(input_line f)
 done
 with End_of_file -> ();;
 
-let bar = (blit_on_char '' x);;
+let bar = (String.split_on_char ' ' !x);;
 
 let rec frequency l=
 match l with
-hd::tl -> (length (filter (f a -> if a = hd then true else false) l))::(frequency tl)
+hd::tl -> ( (length (filter (fun a -> if a = hd then true else false) l)),hd )::(frequency tl)
 |[]->[]                                                                              ;;
 
-print_string hd(rev(fast_sort (f a b if a > b then 1 else if a=b then 0 else -1) (frequency bar)));;                               	
+let y =hd (fast_sort (fun a b -> if a > b then -1 else if a=b then 0 else 1) (frequency bar));;
+print_string ((snd y)^"\n");;
+print_int (fst y);;
