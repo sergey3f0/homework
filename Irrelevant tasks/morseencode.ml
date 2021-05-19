333b182dcfbf61c1ef3ccb5f091c6a79fdd7a801
ZhidomassonFashist <neggneggneg@gmail.com>
2021-05-19 13:34:34 +0300

    commit

--- Irrelevant tasks/morseencode.ml
+++ Irrelevant tasks/morseencode.ml
@@ -1,7 +1,8 @@
 open String;;
+print_string "Введите строку для кодирования: ";;
 let s = read_line();;
 
-let table = [|
+let dict = [
 ('a',"* ***");
 ('b',"*** * * *");
 ('c',"*** * *** *");
@@ -21,22 +22,23 @@ let table = [|
 ('q',"*** *** * ***");
 ('r',"* *** *");
 ('s',"* * *");
-('t',"* ***");
-('u',"* ***");
-('v',"* ***");
-('w',"* ***");
-('x',"* ***");
-('y',"* ***");
-('z',"* ***");
-('a',"* ***");
-(' '," ")|];;
+('t',"***");
+('u',"* * ***");
+('v',"* * * ***");
+('w',"* *** ***");
+('x',"*** * * ***");
+('y',"*** * *** ***");
+('z',"*** *** * *");
+(' '," ")];;
 
 let foo x = if x<0 then 27 else x;;
 
-let rec encode s i =
-if (length s)>(i) then
-(snd (table.(foo((int_of_char (s.[i]))-97))))^"   "^(encode s (i+1))
-else
-"";;
-
-print_string (encode (lowercase_ascii s) 0);;
+let encode s =
+let out = ref "" in
+	for i = 0 to (length s)-1 do
+		out:=!out^"   "^(snd(List.find (fun a -> if ((fst a)=s.[i]) then true else false) dict))
+	done;
+!out;;
+let o = lowercase_ascii (encode s);;
+let o = (sub o 3 ((length o)-3)) ;;
+print_string o;;
