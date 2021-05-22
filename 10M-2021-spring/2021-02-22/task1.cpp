45eadb20236a3352bb9f37cca85f2a4d7deb91fc
sergey3f0 <49240504+sergey3f0@users.noreply.github.com>
2021-05-22 23:31:00 +0300

    забыл что числа должны быть чётными

--- 10M-2021-spring/2021-02-22/task1.cpp
+++ 10M-2021-spring/2021-02-22/task1.cpp
@@ -4,14 +4,14 @@
 int main()
 {
 	std::string s;
-	std::regex rg ("[2-7][0-9]$");
+	std::regex rg ("[2-7][02468]$");
 	std::getline (std::cin,s);
 	if (std::regex_match(s,rg))
 	{
-		std::cout << "String \"" << s <<"\" is a number in range 20-79\n";
+		std::cout << "String \"" << s <<"\" is an even number in range 20-79\n";
 	}
 	else
 	{
-		std::cout << "String \"" << s << "\" is not a number in range 20-79\n";
+		std::cout << "String \"" << s << "\" is not an even number in range 20-79\n";
 	}
 }
